6bb517954534cb2746186aec6e86969918a8b608
EmilyCh0 <67777523+EmilyCh0@users.noreply.github.com>
2021-11-16 22:55:19 +0900

    유저 계정 관련 전체 수정

--- login.php
+++ login.php
@@ -1,127 +1,91 @@
+
 <?php
+require_once "config.php";
 
 session_start();
 
-// 이미 로그인 되어있다면 welcome 페이지로 이동
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    header("location: restlist.php");
     exit;
 }
 
-require_once "config.php";
-
-$username = $password = "";
-$username_err = $password_err = $login_err = "";
-
-// submit 버튼 눌렀을 때
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $password = "";
 
-    // username 입력 확인
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
+    if(empty(trim($_POST["username"]))||empty(trim($_POST["password"]))){
+        echo '<script>alert("Please fill out the forms")</script>';
+    }else{
+        $username = $_POST["username"];
+        $password = $_POST["password"];
     }
 
-    // password 확인
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
 
-    // 입력한 username과 password로 회원 확인
-    if(empty($username_err) && empty($password_err)){
+    if(!empty($username)&&!empty($password)){
+        $sql = "SELECT username, password FROM users WHERE username = '$username'";
+        $res = mysqli_query($mysqli,$sql);
+        if($res){
+            if(mysqli_num_rows($res)==0){
+                // username이 존재하지 않는다.
+                echo '<script>alert("username does not exits")</script>';
+            }else{
+                while($row = mysqli_fetch_assoc($res)){
+                    if($row['password']===$password){
+                        session_start();
 
-        // 실행할 쿼리 - username 확인
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+                        $_SESSION["loggedin"] = true;
+                        $_SESSION["id"] = $id;
+                        $_SESSION["username"] = $username;
 
-        if($stmt = mysqli_prepare($mysqli, $sql)){
-            // stmt에 파라미터 연결
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            // 파라미터에 값 넣기
-            $param_username = $username;
-
-            // stmt 실행
-            if(mysqli_stmt_execute($stmt)){
-
-                mysqli_stmt_store_result($stmt);
-
-                // username이 존재한다면 password를 확인한다.
-                if(mysqli_stmt_num_rows($stmt) == 1){
-
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // 비밀번호 일치
-                            session_start();
-
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
-
-                            // welcome 페이지로 이동
-                            header("location: welcome.php");
-                        } else{
-                            // 틀린 비밀번호
-                            $login_err = "Wrong password. Try again.";
-                        }
+                        header("location: restList.php");
+                    }else{
+                        echo '<script>alert("Wrong password")</script>';
                     }
-                } else{
-                    // username이 존재하지 않는다.
-                    $login_err = "Invalid username";
                 }
-            } else{
-                echo "Error :(";
             }
 
-            mysqli_stmt_close($stmt);
+
         }
+
     }
 
     mysqli_close($mysqli);
+
 }
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <title>Login page</title>
+    <title>Register</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);}
-
+        .container{ width: 400px; padding: 20px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);}
     </style>
+
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p></p>
-
-        <?php
-        if(!empty($login_err)){
-            echo '<div class="alert alert-danger">' . $login_err . '</div>';
-        }
-        ?>
-
+    <div class="container">
+        <h3>Login</h3>
+        <br><br>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group">
                 <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                <input type="text" name="username" class="form-control" >
             </div>
             <div class="form-group">
                 <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                <input type="password" name="password" class="form-control">
             </div>
             <div class="form-group">
-                <input type="submit" class="btn btn-success" value="Login">
+                <input type="submit" name="submit" class="btn btn-success" value="submit">
             </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+            <p>Don't have an account? <a href="register.php">Create Account</a>.</p>
         </form>
     </div>
+
 </body>
+
 </html>
--- reset-password.php
+++ reset-password.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+require_once "config.php";
 
 session_start();
 
@@ -7,86 +8,68 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 
-require_once "config.php";
-
-$new_password = $confirm_password = $new_password_err = $confirm_password_err = "";
-
-// submit 버튼 눌렀을 때
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $newPassword = "";
 
-    if(empty(trim($_POST["new_password"]))){
-        $new_password_err = "Please enter the new password.";
-    } else{
-        $new_password = trim($_POST["new_password"]);
+    if(empty(trim($_POST["newPassword"]))){
+        echo '<script>alert("Please fill out the forms")</script>';
+    }else{
+        $newPassword = $_POST["newPassword"];
     }
 
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm the password.";
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($new_password_err) && ($new_password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
+
+    if(!empty($newPassword)){
+
+        $tmp = $_SESSION["username"];
+
+        $sql = "UPDATE users SET password = '$newPassword' WHERE username = '$tmp'";
+        $res = mysqli_query($mysqli,$sql);
+        if($res){
+            session_destroy();
+            header("location: login.php");
+            exit();
+        }else{
+            echo 'error 1';
+        } 
+
+    }else{
+        echo 'error';
     }
 
-    if(empty($new_password_err) && empty($confirm_password_err)){
-        // update할 쿼리
-        $sql = "UPDATE users SET password = ? WHERE id = ?";
+    mysqli_close($mysqli);
 
-        if($stmt = mysqli_prepare($link, $sql)){
-        
-            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
-            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
-            $param_id = $_SESSION["id"];
-
-            if(mysqli_stmt_execute($stmt)){
-                // update 되면 다시 로그인 페이지로 돌아간다.
-                session_destroy();
-                header("location: login.php");
-                exit();
-            } else{
-                echo "Error :(";
-            }
-
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-    mysqli_close($link);
 }
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <title>Reset Password</title>
+    <title>Register</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);}
+        .container{ width: 400px; padding: 20px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);}
     </style>
+
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Reset Password</h2>
-        <p></p>
+    <div class="container">
+        <h3>Reset Password</h3>
+        <br><br>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group">
                 <label>New Password</label>
-                <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
-                <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
+                <input type="password" name="newPassword" class="form-control" > 
             </div>
+            <br>
             <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-success" value="Submit">
-                <a class="btn btn-link ml-2" href="mypage.php">Cancel</a>
+                <input type="submit" name="submit" class="btn btn-success" value="submit">
             </div>
         </form>
     </div>
+
 </body>
+
 </html>
