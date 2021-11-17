8557e2a7f3bda1d2f23f53b23f7c5f456fac8cb7
jiwon199 <53902714+jiwon199@users.noreply.github.com>
2021-11-18 00:37:30 +0900

    transaction,complex grouping

--- restList_specific.php
+++ restList_specific.php
@@ -18,19 +18,12 @@
 			$restName=$_GET[ 'restName' ];
 			 
             require_once "config.php";
+            //restName
             $sql="select * from restList where restName = '$restName' ";
-             
-           // $mysqli = mysqli_connect("localhost","root","1234","restTest_db");
-            
-        
-            $res = mysqli_query($mysqli,$sql);
-             
-             
+            $res = mysqli_query($mysqli,$sql);            
             if ($res) {
-                $row=mysqli_fetch_array($res);
-                /* echo " wow ".$row['rest_type']." wow "; */
-                $resttype=$row['rest_type'];
-                $ranking=$row['rate'];
+                $row=mysqli_fetch_array($res);              
+                $resttype=$row['resttype'];             
                 $tel=$row['tel'];
                 $location=$row['location'];
                 $addr=$row['addr'];
@@ -38,53 +31,64 @@
             } else {
                 printf("error: %s\n", mysqli_error($mysqli));
             }
-            $sql2= "select * from food where rest = '$restName'";
+            $sql2= "select * from foods where restname = '$restName'";
             $res2 = mysqli_query($mysqli,$sql2);
             
-            $sql3= "select * from comment where rest = '$restName'";
+            $sql3= "select * from reviews where restname = '$restName'";
             $res3 = mysqli_query($mysqli,$sql3);
 
-            
+            //댓글 입력 부분
             if(isset($_POST[ 'contentArea' ])){
+                //transaction 시작 
+                mysqli_begin_transaction($mysqli);
+                 
                 $val=$_POST[ 'contentArea' ];
-                
-                $val2=$val;
-                  // 로그인 구현이 안됐으니 일단 이름은 anon으로 insert 
-                  
+                $score=$_POST['satisfaction'];                        
                 $name=$_SESSION["username"];
-                 
+                  
+                //빈 값이 아닐때만 insert
                 if($val!=null&&$val!=""){  
-
-                    //새로고침시 계속 값 들어가는 걸 막기 위해... 
-                    $sql4= "select * from comment where comment = '$val' and nickname= '$name' and rest= '$restName'" ;
-                    $res4 = mysqli_query($mysqli,$sql4);
-                    if($res4){
-                        $num =mysqli_num_rows($res4);
-                        echo $num;
-                        if($num<1){
-                            $sql5="insert into comment(rest,comment,nickname) values ( '$restName' , '$val' , '$name' )  ";
-                            mysqli_query($mysqli,$sql5); 
-                            echo ("<script>location.href='./restList_specific.php?restName=$restName'</script>") ;
- 
-                        }
-                    }
-                    else{
-                        printf("error: %s\n", mysqli_error($mysqli));
-                    }
-                    
-                     
-                
-                                  
-                }     
-                
                  
-               
+                //같은 사람이 똑같은 리뷰 쓰기 불가능하도록
+                 $duplicate= "select * from reviews where review = '$val' and username= '$name' and restname= '$restName'" ;
+                 $checkdup = mysqli_query($mysqli,$duplicate);
+                 if($checkdup){
+                    $num =mysqli_num_rows($res4);    
+                    if($num<1){
+                             //리뷰를 테이블에 넣기
+                            $sqlInsert="insert into reviews(restname,review,username,score) values ( '$restName' , '$val' , '$name',$score )  ";
+                            $InsertReview=mysqli_query($mysqli,$sqlInsert); 
+                            if($InsertReview){
+                            //성공하면 커밋
+                            mysqli_commit($mysqli);
+                            
+                            //새로고침
+                            echo ("<script>location.href='./restList_specific.php?restName=$restName'</script>") ;
+                            }  
+                            else{
+                                echo "<script> alert('fail');</script>";
+                                //실패시 롤백
+                                mysqli_rollback($mysqli); 
+                            }                                                        
+                    }
+                 }
+                 else{
+                    echo "<script> alert('fail');</script>";
+                    //실패시 롤백
+                    mysqli_rollback($mysqli); 
+                 }
+                                       
+                                   
+                    }  
+         
+              
             }
 
+            //삭제하는 부분 
             if(isset($_POST[ 'deletename' ])&&isset($_POST[ 'deletecontent' ])){
                 $nameD=$_POST[ 'deletename' ];
                 $commentD=$_POST[ 'deletecontent' ];
-                $delete="delete from comment where nickname= '$nameD' and comment='$commentD'";
+                $delete="delete from reviews where username= '$nameD' and review='$commentD'";
                 $deleteRes = mysqli_query($mysqli,$delete); 
                 if($deleteRes){
                     echo "<script> alert('delete complete');</script>";
@@ -111,8 +115,7 @@
          <div class="specificInfo">
 
 
-             <?php
-             echo   "rate: $ranking <br> "; 
+             <?php          
              echo   "location: $location <br>";
              echo   "address: $addr <br>";
              echo   "contact: $tel <br> <br>"; 
@@ -121,20 +124,52 @@
              if($res2){
                 while($newArray=mysqli_fetch_array($res2,MYSQLI_ASSOC)){
                     $food=$newArray['food'];
-                    echo " $food <br>" ;
+                    $price=$newArray['price'];
+                    echo "$food : $price won<br>";
                 }
             }else{
                 printf("error" ); 
             }
-           
+            echo "<br>------------ employee ------------<br>";
+           $sqlEmployee="select restname, count(*),role from employee group by restname, role having restname= '$restName';";
+           $EmployRes = mysqli_query($mysqli,$sqlEmployee);
+           if($EmployRes){
+            while($newArray=mysqli_fetch_array($EmployRes,MYSQLI_ASSOC)){
+                $role=$newArray['role'];
+                $peopleNum=$newArray['count(*)'];
+                echo "$role : $peopleNum persons<br>";
+            }
+           }
+           else{
+            printf("error" ); 
+           }
                ?>
 
          </div>
 
          <div class="commentArea">
-             comment 
+
+
+             <br>
+
+             <form action="restList_specific.php?restName=<?php echo $restName;?>&' " method=post>
+
+
+                 <select name='satisfaction' id='satifaction'>
+                     <option value=5 selected>★★★★★
+                     <option value=4> ★★★★
+                     <option value=3>★★★
+                     <option value=2> ★★
+                     <option value=1> ★
+                 </select>
+
+                 <input type="textarea" class='contentArea' id='contentArea' name='contentArea' maxlength=130>
+                 <input type="submit" id=rep_bt class="btn btn-success" value="submit">
+             </form>
+
+             <br>comment
              <?php
-             $commentN="SELECT rest,COUNT(comment) AS commentNum FROM comment GROUP BY rest having rest= '$restName'; ";
+             $commentN="SELECT restname,COUNT(review) AS commentNum FROM reviews GROUP BY restname having restname= '$restName'; ";
              $countRes = mysqli_query($mysqli,$commentN); 
              if($commentN){
                 while($newArray=mysqli_fetch_array($countRes,MYSQLI_ASSOC)){
@@ -144,17 +179,7 @@
             }else{
                 printf("error" ); 
             }
-             ?> 
-             <br>
-
-             <form action="restList_specific.php?restName=<?php echo $restName;?>&' " method=post>
-
-                 <input type="textarea" class='contentArea' id='contentArea' name='contentArea' maxlength=130>
-                 <input type="submit" id=rep_bt class=re_bt value="submit">
-             </form>
-
-
-
+             ?>
 
              <table class="type22">
 
@@ -165,10 +190,17 @@
                  }
               if($res3){
                 while($newArray=mysqli_fetch_array($res3,MYSQLI_ASSOC)){
-                    $name=$newArray['nickname'];
-                    $comment=$newArray['comment'];     
+                    $name=$newArray['username'];
+                    $comment=$newArray['review'];     
+                    $score=$newArray['score']; 
+                    if($score==5) $star="★★★★★";
+                    else if($score==4) $star="★★★★";
+                    else if($score==3) $star="★★★";
+                    else if($score==2) $star="★★";
+                    else  $star="★";
                                    
-                    echo "<tr> <td> <b> $name </b>";               
+                    echo "<tr> <td> <b> $name </b>";    
+                    echo " <span id='scoreText'>   $star  </span> " ;           
                     if($name==$_SESSION["username"]){ 
                     ?>
 
@@ -181,10 +213,8 @@
                      <input type='hidden' name='comment' value='$comment'>
                      <input type='hidden' name='name' value='$name'>
                      <input type='submit' id='updateBtn' value='update'>
-                 </form>";
-?>
-
-
+                        </form>";
+                    ?>
 
                      <form action="restList_specific.php?restName=<?php echo $restName;?>&' " method=post>
 
@@ -193,11 +223,11 @@
                          <input type="submit" id=deleteBtn class=deleteBtn value="delete">
                      </form>
 
-
-
                      <?php
                      }
+                     
                     echo " <br> $comment </td></tr>" ;
+                    
                  }
                  }else{
                  printf("error" );
