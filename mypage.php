e29e08c9e25ccbb0c2d9fb5b120043c070c6a814
EmilyCh0 <67777523+EmilyCh0@users.noreply.github.com>
2021-11-17 22:09:05 +0900

    my info 추가

--- mypage.php
+++ mypage.php
@@ -8,6 +8,19 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
+$username = $_SESSION["username"];
+$location = $contact = "";
+$sql = "SELECT address, contact FROM userDetails WHERE username = '$username'";
+$res = mysqli_query($mysqli,$sql);
+
+if($res){
+    while($row = mysqli_fetch_assoc($res)){
+        $location = $row['address'];
+        $contact = $row['contact'];
+    }
+    
+}
+
 ?>
 
 
@@ -19,14 +32,19 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; text-align: center; }
-        .wrapper{ width: 100%; padding: 20px; margin-left: auto; margin-right: auto;}
+        .wrapper{ width: 100%; padding: 20px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);}
         .table{ width:500px; margin-left: auto; margin-right: auto;}
     </style>
 
 </head>
 <body>
     <div class="wrapper">
-        <h1 class="my-5">This is <?php echo htmlspecialchars($_SESSION["username"]); ?>'s Page</h1>
+        <h1 class="my-5"><?php echo htmlspecialchars($_SESSION["username"]); ?>'s Page</h1>
+        <h4>My Info.</h4>
+        <h6>Location: <?php echo $location ?></h6>
+        <h6>Contact: <?php echo $contact ?></h6>
+        <br><br>
+
         <a href="restlist.php" class="btn btn-primary">Back to main</a>
         <p></p>
         <a href="reset-password.php" class="btn btn-outline-secondary">Reset Password</a>
